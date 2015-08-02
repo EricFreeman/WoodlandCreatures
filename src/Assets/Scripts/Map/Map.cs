eb254d27a0ec3ce2094a81915369853c0190d875
Eric Freeman <clownhunter@gmail.com>
2015-08-01 20:16:21 -0600

    Rip apart spritesheets into separate textures.

---
+++ src/Assets/Scripts/Map/Map.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Map
+{
+    public class Map : MonoBehaviour
+    {
+        public Texture2D SpriteSheet;
+        public int TileSize;
+
+        private Texture2D[] _atlastTexture;
+
+        void Start()
+        {
+            var width = SpriteSheet.width / TileSize;
+            var height = SpriteSheet.height / TileSize;
+
+            _atlastTexture = new Texture2D[width * height];
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    var textureColor = SpriteSheet.GetPixels(x * TileSize, y * TileSize, TileSize, TileSize);
+                    var newTexture = new Texture2D(TileSize, TileSize);
+                    newTexture.SetPixels(0, 0, TileSize, TileSize, textureColor);
+
+                    _atlastTexture[x + y * width] = newTexture;
+                }
+            }
+        }
+
+        public Texture2D GetTexture(int index)
+        {
+            return _atlastTexture[index];
+        }
+    }
+}
