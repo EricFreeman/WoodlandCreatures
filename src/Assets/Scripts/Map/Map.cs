c490a12715388699795ea76e969d7840e07894aa
Eric Freeman <clownhunter@gmail.com>
2015-08-03 20:36:29 -0600

    Fixed issue with Unity reading in textures upside down.  Tested it out by applying texture to box.

--- src/Assets/Scripts/Map/Map.cs
+++ src/Assets/Scripts/Map/Map.cs
@@ -12,27 +12,36 @@ namespace Assets.Scripts.Map
 
         void Start()
         {
-            var width = SpriteSheet.width / TileWidth;
-            var height = SpriteSheet.height / TileHeight;
+            GenerateSpriteSheet();
 
-            _atlastTexture = new Texture2D[width * height];
+            GetComponentInChildren<Renderer>()
+                .material.SetTexture("_MainTex", GetTexture(0));
+        }
+
+        public Texture GetTexture(int index)
+        {
+            return _atlastTexture[index];
+        }
+
+        private void GenerateSpriteSheet()
+        {
+            var width = SpriteSheet.width/TileWidth;
+            var height = SpriteSheet.height/TileHeight;
+
+            _atlastTexture = new Texture2D[width*height];
 
             for (var y = 0; y < height; y++)
             {
                 for (var x = 0; x < width; x++)
                 {
-                    var textureColor = SpriteSheet.GetPixels(x * TileWidth, y * TileHeight, TileWidth, TileHeight);
+                    var textureColor = SpriteSheet.GetPixels(x*TileWidth, y*TileHeight, TileWidth, TileHeight);
                     var newTexture = new Texture2D(TileWidth, TileHeight);
                     newTexture.SetPixels(0, 0, TileWidth, TileHeight, textureColor);
 
-                    _atlastTexture[x + y * width] = newTexture;
+                    // Unity reads textures from bottom to top instead of top to bottom
+                    _atlastTexture[x + (height - 1 - y)*width] = newTexture;
                 }
             }
-        }
-
-        public Texture2D GetTexture(int index)
-        {
-            return _atlastTexture[index];
         }
     }
 }
