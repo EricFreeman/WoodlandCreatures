7af5c1617c2df7b1d4108aba53256a60f54d4158
Eric Freeman <efreeman@1800contacts.com>
2015-08-04 13:18:10 -0600

    Refactored sprite stuff out into its own class (so I can pull it into a separate library soon).

--- src/Assets/Scripts/Map/Map.cs
+++ src/Assets/Scripts/Map/Map.cs
@@ -4,44 +4,15 @@ namespace Assets.Scripts.Map
 {
     public class Map : MonoBehaviour
     {
-        public Texture2D SpriteSheet;
-        public int TileWidth;
-        public int TileHeight;
-
-        private Texture2D[] _atlastTexture;
-
         void Start()
         {
-            GenerateSpriteSheet();
+            var spriteSheet = GetComponent<SpriteSheet>();
+            spriteSheet.Sheet = Resources.Load<Texture2D>("Images/Sheets/Sheet1");
+            spriteSheet.TileWidth = 32;
+            spriteSheet.TileHeight = 32;
+            spriteSheet.GenerateSpriteSheet();
 
-            GetComponentInChildren<Renderer>()
-                .material.SetTexture("_MainTex", GetTexture(0));
-        }
-
-        public Texture GetTexture(int index)
-        {
-            return _atlastTexture[index];
-        }
-
-        private void GenerateSpriteSheet()
-        {
-            var width = SpriteSheet.width/TileWidth;
-            var height = SpriteSheet.height/TileHeight;
-
-            _atlastTexture = new Texture2D[width*height];
-
-            for (var y = 0; y < height; y++)
-            {
-                for (var x = 0; x < width; x++)
-                {
-                    var textureColor = SpriteSheet.GetPixels(x*TileWidth, y*TileHeight, TileWidth, TileHeight);
-                    var newTexture = new Texture2D(TileWidth, TileHeight);
-                    newTexture.SetPixels(0, 0, TileWidth, TileHeight, textureColor);
-
-                    // Unity reads textures from bottom to top instead of top to bottom
-                    _atlastTexture[x + (height - 1 - y)*width] = newTexture;
-                }
-            }
+            GetComponentsInChildren<Transform>()[1].GetComponent<Renderer>().material.SetTexture("_MainTex", spriteSheet.GetTexture(0));
         }
     }
 }
